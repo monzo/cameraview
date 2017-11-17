c72f1660a005d3bf8487da8da9bba17007052ff9
Ivan <ivan@monzo.com>
2017-11-17 16:53:02 +0000

    Remove empty lines

--- library/src/main/base/com/google/android/cameraview/AspectRatio.java
+++ library/src/main/base/com/google/android/cameraview/AspectRatio.java
@@ -137,8 +137,6 @@ public class AspectRatio implements Comparable<AspectRatio>, Parcelable {
         return (float) mX / mY;
     }
 
-
-
     @Override
     public int hashCode() {
         // assuming most sizes are <2^16, doing a rotate will give us perfect hashing
